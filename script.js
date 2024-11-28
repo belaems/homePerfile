cdbf7b093d550abdeff7047cedbf7b5007463fca
Isabela <104663212+belaems@users.noreply.github.com>
2024-11-28 03:16:46 +0000

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Função para abrir o menu hamburger */
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
